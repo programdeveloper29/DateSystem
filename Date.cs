c9f61ec6c9277a6f5e042076e03aaa20673c848e
H. Ahmed <programdeveloper29@gmail.com>
2026-03-24 12:52:41 +0200

    This project is a simple Date Management Console Application built using C#.

---
+++ Date.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date
+{
+    internal class Date
+    {      //field                                         //  0,1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10,11,12
+        private static readonly sbyte[] DaysToMonth365 = {0,31,28,31,30,31,30,31,31,30,31,30,31,30,31};
+        private static readonly sbyte[] DaysToMonth366 = {0,31,29,31,30,31,30,31,31,30,31,30,31,30,30,31};
+
+        private readonly sbyte day = 01;
+        private readonly sbyte month = 01;
+        private readonly uint year=01;
+        //constractor
+        public Date(sbyte day, sbyte month, uint year)
+        {
+            var isleap = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0); ;
+            
+            if(year>=1&&year<=9999&&month>=1&&month<=12)
+            {
+                sbyte[] days = isleap ? DaysToMonth366 : DaysToMonth365;
+                if(day>=1 && day<=days[month])
+                {
+                    this.day = day;
+                    this.month = month;
+                    this.year = year;
+
+                }
+            }
+            
+        }
+        //overload Constructor1
+        public Date(sbyte month, uint year) : this(01, month, year) { }
+        //overload constructor2
+        public Date(uint year):this(01,01,year) { }
+        public Date()
+        {
+            day = 01;
+            month = 01;
+            year = 01;
+        }
+      public string GetDate()
+        {
+            return $"{day.ToString().PadLeft(2,'0')}/{month.ToString().PadLeft(2,'0')}/{year.ToString().PadLeft(4,'0')}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte num;
+            int n;
+            char ch;
+            Console.WriteLine("\n******** Date********\n");
+            do { 
+            Console.Write("\n\r1=>Full Date.\n\r2=>Month&Year.\n\r3=>Year Only.\n\rSelect:");
+            string input= Console.ReadLine();
+            Console.Write("Number of Dates:");
+            string input1 = Console.ReadLine();
+            if (sbyte.TryParse(input, out num)&& int.TryParse(input1, out  n))
+            {
+                switch (num) {
+                    case 1:
+
+                        PrintDate(CreateFullDate(n));
+                    break;
+
+                    case 2: 
+                        PrintDate(MonthYearDate(n));
+
+                    break;
+                    case 3:
+                        PrintDate(YearDate(n));
+                    break;
+                    default:
+                        Date date = new Date();
+                        Console.Write(date.GetDate());
+
+                     break;
+                }
+            }
+            Console.WriteLine("\n********************************\n");
+                Console.Write("\nRepeate('y'|'n')? ");
+                ch = char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine("\n********************************\n");
+            } while (ch.Equals('y'));
+        }
+        //Mehtods To insert Date
+        static Date[] CreateFullDate(int n)
+            {
+            Console.WriteLine("\n********Enter Full Date********\n");
+            Date[] date = new Date[n];
+            for(int i=0;i<n;i++)
+            {Console.Write("Day:");
+            string input1=Console.ReadLine();
+            Console.Write("Month:");
+            string input2=Console.ReadLine();
+            Console.Write("Year:");
+            string input3 = Console.ReadLine();
+                if (sbyte.TryParse(input1, out sbyte day) && sbyte.TryParse(input2, out sbyte month) && uint.TryParse(input3, out uint year))
+                    date[i] = new Date(day, month, year);
+                else Console.WriteLine("Invalid Value");
+
+            }
+            
+
+            
+            
+            
+            return date; }
+        static Date[] MonthYearDate(int n)
+        {
+            Console.WriteLine("\n********Enter Month&Year Of Date********\n");
+            Date[] date = new Date[n];
+                for (int i = 0; i < n; i++)
+                {
+                   
+                    Console.Write("Month:");
+                    string input2 = Console.ReadLine();
+                    Console.Write("Year:");
+                    string input3 = Console.ReadLine();
+                    if (sbyte.TryParse(input2, out sbyte month) && uint.TryParse(input3, out uint year))
+                        date[i] = new Date(month, year);
+                    else Console.WriteLine("Invalid Value");
+
+                }
+                return date; }
+        static Date[] YearDate(int n)
+        {
+            Console.WriteLine("\n********Enter Year Of Date********\n");
+            Date[] date = new Date[n];
+            for (int i = 0; i < n; i++)
+            {
+          
+                Console.Write("Year:");
+                string input3 = Console.ReadLine();
+                if (uint.TryParse(input3, out uint year))
+                    date[i] = new Date(year);
+                else Console.WriteLine("Invalid Value");
+
+            }
+            return date;
+        }
+        //Method To Print Data
+        static void PrintDate(Date[] dates)
+        {
+            Console.WriteLine("\n********Print Date********\n");
+            int i = 1;
+            foreach (Date date in dates)
+
+           { Console.WriteLine($"Date[{i}]: {date.GetDate()}"); i++; }
+        }
+    }
+}
